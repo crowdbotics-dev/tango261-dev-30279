ccf741c11781e4b856508eb62e322bef02578dc6
Crowdbotics <engineering@crowdbotics.com>
2022-08-25 06:04:38 +0000

    Initial commit.

---
+++ android/app/src/main/java/com/tango261_dev_30279/MainActivity.java
@@ -0,0 +1,15 @@
+package com.tango261_dev_30279;
+
+import com.facebook.react.ReactActivity;
+
+public class MainActivity extends ReactActivity {
+
+  /**
+   * Returns the name of the main component registered from JavaScript. This is used to schedule
+   * rendering of the component.
+   */
+  @Override
+  protected String getMainComponentName() {
+    return "tango261_dev_30279";
+  }
+}
